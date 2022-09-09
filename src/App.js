6388add7001d3cbc7f437987d52921aacafb5215
Av <Kirankumar.Av@lumen.com>
2022-09-09 17:44:47 +0530

    newfile

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import Handle from './Component/EnvironmentButtons'
+import HErrorBoundary from './Component/HErrorBoundary'
+
+import Pocdetails from './Component/Pocdetails';
+import Push3 from './Component/Pushapp'
+
+import Homelinkverify from './Component/HomeLinkTest'
+
+
+import Supportselect from './Component/DataFetchinGFinalSelectiontest'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div><Homelinkverify/></div> <br></br>
+        <div className="flex1">
+       &nbsp;&nbsp; <HErrorBoundary><Handle/></HErrorBoundary>&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; {/* used  <Push3/> */} <Pocdetails/> &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+         
+        &nbsp;&nbsp; &nbsp;&nbsp;  &nbsp;&nbsp; &nbsp;&nbsp;  <Supportselect/>  <br></br>   
+
+    </div>
       </header>
     </div>
   );
---
+++ src/Component/HomeLinkTest.js
@@ -0,0 +1,16 @@
+import React from 'react'
+export default class Homelinkverify extends React.Component{
+    render(){
+        return(
+            <div> <br></br>
+                     
+                    &nbsp; <img className="lumen" 
+                                        src="https://assets.lumen.com/is/content/Lumen/Lumen-logo-R?Creativeid=dd032ee8-6530-4437-8e9d-209b357060ce"
+                                        alt="Lumen Logo"></img>
+                      
+                     
+            </div>
+       
+        )
+    }
+}
